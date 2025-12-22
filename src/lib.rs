6b66e7124436f0507224cba95ff7b758389eaeb7
Fredrik Willumsen Haug <fredrikwhaug@gmail.com>
2025-12-22 12:44:45 +0100

    Method implementations for CircularBuffer.

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+#![no_std]
+
+use core::mem::MaybeUninit;
+
+pub struct CircularBuffer<T, const N: usize> {
+    buffer: [MaybeUninit<T>; N],
+    head: usize,
+    tail: usize,
+    len: usize,
+}
+
+impl<T, const N: usize> CircularBuffer<T, N> {
+    pub fn new() -> Self {
+        Self {
+            buffer: unsafe { MaybeUninit::uninit().assume_init() },
+            head: 0,
+            tail: 0,
+            len: 0,
+        }
+    }
+
+    pub fn push(&mut self, value: T) -> Result<(), T> {
+        if self.len == N {
+            return Err(value);
+        }
+
+        self.buffer[self.head].write(value);
+        self.head = (self.head + 1) % N;
+        self.len += 1;
+        Ok(())
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None;
+        }
+
+        let value = unsafe { self.buffer[self.tail].assume_init_read() };
+        self.tail = (self.tail + 1) % N;
+        self.len -= 1;
+        Some(value)
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.len == N
+    }
+}
